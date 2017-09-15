0911c9f083021b87f506308f48e56fe55f0b164c
Alexander Nanberg <alex.nanberg@hotmail.com>
2017-09-15 13:28:51 +0200

    Profile view

---
+++ src/components/ProfileView.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './common/Section'
+import BaseContainer from '../components/common/Container'
+import Button from '../components/common/Button'
+import Title from '../components/common/Title'
+import Text from '../components/common/Text'
+import { colors } from '../constants'
+
+const Container = BaseContainer.extend`
+  display: grid;
+  grid-gap: 32px 12px;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto;
+  justify-items: center;
+  justify-content: center;
+`
+
+const Avatar = styled.img`
+  width: 88px;
+  height: 88px;
+  border-radius: 50%;
+`
+
+const Details = styled.div`
+  justify-self: center;
+  align-self: center;
+`
+
+const Email = styled(Text)`
+  font-size: 1.3rem;
+  color: ${colors.gray600}
+`
+
+const ProfileView = ({ user }) => (
+  <Section>
+    <Container>
+      <Avatar src={`${user.gravatar}?s=200`} alt={user.name} />
+      <Details>
+        <Title.H1>{user.name}</Title.H1>
+        <Email>{user.email}</Email>
+      </Details>
+      <Button onClick={this.handleLogout}>Logga ut</Button>
+    </Container>
+  </Section>
+)
+
+export default ProfileView
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
-import Button from '../components/common/Button'
-import Title from '../components/common/Title'
-import Section from '../components/common/Section'
-import Container from '../components/common/Container'
-import Text from '../components/common/Text'
+import ProfileView from '../components/ProfileView'
 
 @inject('userStore')
 @observer
@@ -17,15 +13,9 @@ class Profile extends React.Component {
     const { user } = this.props.userStore
 
     return (
-      <Section>
-        <Container>
-          <img src={user.gravatar} alt={user.name} />
-          <Title.H1>{user.name}</Title.H1>
-          <Text>Id: {user.id}</Text>
-          <Text>Epost: {user.email}</Text>
-          <Button onClick={this.handleLogout}>Logga ut</Button>
-        </Container>
-      </Section>
+      <div>
+        <ProfileView user={user} />
+      </div>
     )
   }
 }
