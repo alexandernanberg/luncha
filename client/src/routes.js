872a222eca131d9e1da2bb707516609cbb891ce9
Alexander Nanberg <alex.nanberg@hotmail.com>
2017-11-24 16:03:05 +0100

    Add title values

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,7 @@ import { v4 } from 'uuid'
 import Home from './pages/Home'
 import Archive from './pages/Archive'
 import Recipe from './pages/Recipe'
-import NotFound from './pages/NotFound'
+import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
@@ -43,7 +43,7 @@ const routes = [
   },
   {
     key: v4(),
-    component: NotFound,
+    component: PageNotFound,
   },
 ]
 
