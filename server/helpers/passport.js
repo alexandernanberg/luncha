763b990586b135de38443133c3432f6d205ed45e
Alexander Nanberg <alex.nanberg@hotmail.com>
2019-05-28 21:47:44 +0200

    Fix passport

--- server/helpers/passport.js
+++ server/helpers/passport.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose')
 const passport = require('passport')
-// const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt')
+const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt')
 
 const User = mongoose.model('User')
 
 passport.use(User.createStrategy())
-// passport.use(
-//   new JwtStrategy(
-//     {
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: process.env.SECRET,
-//     },
-//     async (payload, done) => {
-//       const user = await User.findOne({ _id: payload.id })
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET,
+    },
+    async (payload, done) => {
+      const user = await User.findOne({ _id: payload.id })
 
-//       if (user) {
-//         return done(null, user)
-//       }
+      if (user) {
+        return done(null, user)
+      }
 
-//       return done(null, false)
-//     },
-//   ),
-// )
+      return done(null, false)
+    },
+  ),
+)
 
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
